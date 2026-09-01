6ad1ac7d6b13398e435c8077fb9bdb43dee250e8
Özgüç Bayrak <ozgucbayrak@Ozgucs-Mac-mini.local>
2026-09-01 23:15:27 +0300

    Yeni anasayfa formati, css import

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,4 @@
-// @ts-check
+/ @ts-check
 import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
@@ -16,7 +16,7 @@ export default defineConfig({
       platformProxy: {
           enabled: true,
       },
-	}),
+        }),
 
   vite: {
     plugins: [tailwindcss()],
