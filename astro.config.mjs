0a22bbeeec0e2731448e3e952880e51881dc8b54
Özgüç Bayrak <ozgucbayrak@Ozgucs-Mac-mini.local>
2026-09-01 23:17:24 +0300

    Yeni anasayfa formati, css import

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,7 +9,7 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://example.com",
+  site: "https://gokbilim.net",
   integrations: [mdx(), sitemap()],
 
   adapter: cloudflare({
