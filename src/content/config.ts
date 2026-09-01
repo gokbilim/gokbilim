0cc98ec225f867b96b4b28e447509e0e09a60a88
Özgüç Bayrak <ozgucbayrak@Ozgucs-Mac-mini.local>
2026-09-02 00:43:10 +0300

    Yeni anasayfa formati, config.ts eklendi

---
+++ src/content/config.ts
@@ -0,0 +1,15 @@
+import { defineCollection, z } from 'astro:content';
+
+const haberler = defineCollection({
+  type: 'content',
+  schema: z.object({
+    title: z.string(),
+    pubDate: z.coerce.date(),
+    importance: z.string().optional(),
+    image: z.string().optional(),
+  }),
+});
+
+export const collections = {
+  'haberler': haberler,
+};
